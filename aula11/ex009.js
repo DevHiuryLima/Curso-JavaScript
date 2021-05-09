34b338e487ab7200e90c55311d9db4d6343d3893
Hiury Lima <limahiury6@gmail.com>
2021-05-09 12:35:30 -0300

    Exercício
    Condições (Parte 1)

---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'BRASIL'
+
+console.log(`Vivendo em ${pais}`);
+
+if (pais != 'Brasil' && pais != 'BRASIL') {
+    console.log('Você é Extrangeiro!');
+} else {
+    console.log('Você é Brasileiro!');
+}
