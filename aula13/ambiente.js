21c5fb5871aaefda65c1f077c3f6e27d00654c9d
Hiury Lima <limahiury6@gmail.com>
2021-05-11 17:32:05 -0300

    Exercício
    Repetições (Parte 1), Utilizando as estruturas while e do..while.

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var contador = 1
+
+// while (contador <= 6) {
+//     console.log(`Passo ${contador}`);
+//     contador++;
+// }
+
+do {
+    console.log(`Passo ${contador}`);
+    contador++;
+}while(contador <= 6)
