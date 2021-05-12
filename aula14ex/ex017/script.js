e9bcff0e551178e8bf2f2f889cbd90d3240569a2
Hiury Lima <limahiury6@gmail.com>
2021-05-11 23:17:44 -0300

    Exercício
    Exercícios JavaScript (Parte 6). Tabuada.

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,21 @@
+function tabuada() {
+    let num =document.getElementById('txtn');
+    let tab = document.getElementById('seltab');
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!');
+    } else {
+        let n = Number(num.value);
+        let i = 1;
+        tab.innerHTML = '';
+        
+        while (i<=10) {
+            let item = document.createElement('option');
+            item.text = `${n} x ${i} = ${n * i}`;
+            item.value = `tab${i}`;
+            tab.appendChild(item);
+            i++;
+        }
+        
+    }
+}
